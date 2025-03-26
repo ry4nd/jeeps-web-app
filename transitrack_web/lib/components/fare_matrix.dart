e82d09bac42cbe0e16472c30f4ce8ef1cf558af2
James Ducay <jpducay@up.edu.ph>
2025-03-26 17:06:26 +0800

    feat: dialog UI

--- transitrack_web/lib/components/fare_matrix.dart
+++ transitrack_web/lib/components/fare_matrix.dart
@@ -1,4 +1,6 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:pointer_interceptor/pointer_interceptor.dart';
 import 'package:transitrack_web/models/route_model.dart';
 import 'package:transitrack_web/style/constants.dart';
 
@@ -11,6 +13,101 @@ class FareMatrix extends StatefulWidget {
 }
 
 class FareMatrixState extends State<FareMatrix> {
+  int computedFarePrice = 0;
+  int discountedFarePrice = 0;
+  String _selectedFrom = 'Location A'; // Default value for "From"
+  String _selectedTo = 'Location A'; // Default value for "To"
+  final List<String> _locations = [
+    'Location A',
+    'Location B',
+    'Location C'
+  ]; // Dropdown options
+
+  Widget buildDropdown({
+    required String label,
+    required String selectedValue,
+    required ValueChanged<String?> onChanged,
+  }) {
+    return DropdownMenu(
+      label: Text(label),
+      width: 350,
+      enableFilter: true,
+      initialSelection: selectedValue,
+      onSelected: (newValue) {
+        if (newValue is String) {
+          onChanged(newValue);
+        }
+      },
+      dropdownMenuEntries: _locations.map((String value) {
+        return DropdownMenuEntry<String>(
+          value: value,
+          label: value,
+        );
+      }).toList(),
+    );
+  }
+
+  void showFareMatrix() {
+    AwesomeDialog(
+        context: context,
+        dialogType: DialogType.noHeader,
+        width: 450,
+        alignment: Alignment.topCenter,
+        body: PointerInterceptor(
+            child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Fare Matrix',
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.bold)),
+                        IconButton(
+                          icon: const Icon(Icons.close, color: Colors.white),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ]),
+                  SizedBox(height: Constants.defaultPadding / 2),
+                  const Divider(color: Colors.white),
+                  SizedBox(height: Constants.defaultPadding),
+                  buildDropdown(
+                    label: 'From',
+                    selectedValue: _selectedFrom,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedFrom = newValue!;
+                      });
+                    },
+                  ),
+                  SizedBox(height: Constants.defaultPadding),
+                  buildDropdown(
+                    label: 'To',
+                    selectedValue: _selectedTo,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedTo = newValue!;
+                      });
+                    },
+                  ),
+                  SizedBox(height: Constants.defaultPadding * 2),
+                  Text('Estimated Trip Fare: $computedFarePrice',
+                      style: TextStyle(fontSize: 16)),
+                  Text('Discounted Trip Fare: $discountedFarePrice',
+                      style: TextStyle(fontSize: 16)),
+                  SizedBox(height: Constants.defaultPadding * 2),
+                ],
+              ),
+            )
+          ],
+        ))).show();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(children: [
@@ -26,22 +123,7 @@ class FareMatrixState extends State<FareMatrix> {
           ),
         ),
         onPressed: () {
-          // Add your logic here to navigate or display the fare matrix
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: const Text('Fare Matrix'),
-                content: const Text('Display fare matrix details here.'),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: const Text('Close'),
-                  ),
-                ],
-              );
-            },
-          );
+          showFareMatrix();
         },
         child: const Text(
           'View Fare Matrix',
